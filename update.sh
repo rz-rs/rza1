0f543f99418f00e82e28d25bf20577ec816c5a36
yvt <i@yvt.jp>
2020-09-04 02:29:42 +0900

    chore(deps): remove the dependency on `bare-metal`

--- update.sh
+++ update.sh
@@ -10,7 +10,12 @@ form -i lib.rs -o src/ && rm lib.rs
 cargo fmt
 
 # allow `use ...::Deref` to be unused
-sed -E -i '' 's/#!\[deny\(warnings\)\]//' src/lib.rs
+sed -E -i.bak 's/#!\[deny\(warnings\)\]//' src/lib.rs
+rm src/lib.rs.bak
+
+# remove `bare-metal` dependency
+sed -E -i.bak 's/extern crate bare_metal;//' src/lib.rs
+rm src/lib.rs.bak
 
 # feature-gate each module
 ./scripts/featureify.py src/lib.rs
